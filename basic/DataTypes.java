523a1d2110e036d182df6fbca4b210091075bcfe
Sivakarthika29 <sivakarthika.sekar29@gmail.com>
2023-09-26 19:02:31 +0530

    commit

---
+++ basic/DataTypes.java
@@ -0,0 +1,21 @@
+public class DataTypes{
+    public static void main(String[] args) {
+        
+    }
+}
+
+
+/*   00111001010101
+Datatype -> Primitive , Non Primitive
+Primitive:
+    boolean     - 1 bit - false
+    byte        
+    char 
+    short 
+    int 
+    long 
+    float 
+    double 
+Non primitive:
+    String , Arrays ..etc
+ */
